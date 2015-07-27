c3c7b3627aa391fae16a164b16988edad3c32cbe
Janne Vikstedt <janne.vikstedt@gmail.com>
2015-07-28 00:31:17 +0300

    Added vim install scripts

--- vim/install.sh
+++ vim/install.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-echo "Installing vim..."
+echo "Installing vim and prerequisites..."
+
+echo "Installing vim configs"
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+if [ -f ~/.vimrc ];
+then
+  echo "Found existing vimrc, skipping vimrc linking";
+else
+  echo "Linking vimrc"
+  ln -s ${DIR}/vimrc ~/.vimrc
+fi
+
+if [ -d ~/.vim/ ];
+then
+  echo "Found existing vim folder, skipping linking";
+else
+  echo "Linking .vim"
+  ln -s ${DIR} ~/.vim
+fi
