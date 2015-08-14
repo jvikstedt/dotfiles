7634e14c721b13a4aea40034d8b78e92a7689f25
Janne Vikstedt <vikstedt.janne@gmail.com>
2015-08-14 13:03:51 -0700

    Added synapse launcher install to apps install script

--- apps/install.sh
+++ apps/install.sh
@@ -2,6 +2,8 @@
 
 echo "Installing apps"
 
+sudo apt-add-repository ppa:synapse-core/testing
+sudo apt-get update
 sudo apt-get install silversearcher-ag
 sudo apt-get install exuberant-ctags
 sudo apt-get install xclip
@@ -9,3 +11,4 @@ sudo apt-get install curl
 sudo apt-get install vim-gtk
 sudo apt-get install build-essential cmake
 sudo apt-get install python-dev
+sudo apt-get install synapse
