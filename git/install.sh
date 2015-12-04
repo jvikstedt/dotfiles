f1cadc480dea1b3bb4de1ae23633ec5acdb718ef
Janne Vikstedt <janne.vikstedt@gmail.com>
2015-12-04 13:01:01 +0200

    Git alias for glog

--- git/install.sh
+++ git/install.sh
@@ -8,3 +8,4 @@ echo "Setting up global git configs and git template"
 
 git config --global core.excludesfile '~/dotfiles/git/gitignore_global'
 git config --global init.templatedir '~/dotfiles/git/git_template'
+git config --global alias.glog "log --all --pretty='format:%d %Cgreen%h%Creset %an - %s' --graph"
