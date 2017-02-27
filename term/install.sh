0320ff1fae7e72b21cdf5b03e30ebfba38b73e51
Janne Vikstedt <vikstedt.janne@gmail.com>
2017-02-27 12:26:12 +0200

    alt -key fix for xterm

--- term/install.sh
+++ term/install.sh
@@ -11,3 +11,12 @@ else
   echo "Linking Xdefaults"
   ln -s ${DIR}/Xdefaults ~/.Xdefaults
 fi
+
+
+if [ -d ~/.inputrc/ ];
+then
+  echo "Found existing inputrc config, skipping linking";
+else
+  echo "Linking .inputrc"
+  ln -s ${DIR}/inputrc ~/.inputrc
+fi
